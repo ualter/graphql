bb54addc60f5c5eed375412e192f3a3ae291efcc
Ualter Azambuja Junior <ualter.junior@gmail.com>
2019-10-02 12:32:27 +0200

    .

---
+++ books-springboot/src/main/java/com/graphqljava/tutorial/booksspringboot/BooksSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.booksspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksSpringbootApplication.class, args);
+	}
+
+}
